8281aa7a22e40bc2da936d4bf77fa8732b5fd946
Abhay Sharma <Abhay.Sharma@course5i.com>
2024-09-04 15:58:33 +0530

    MLOps initial architecture setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='MLOps implementation for CCSC Why Calls',
+    author='C5i',
+    license='',
+)
